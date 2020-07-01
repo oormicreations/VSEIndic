f487924d5e0b582bbf11195c00396207d9467d4d
Oormi Creations <oormicreations@gmail.com>
2020-07-01 22:33:13 +0530

    Ver 0.2.1

--- VSEIndic.py
+++ VSEIndic.py
@@ -6,7 +6,7 @@ bl_info = {
     "name": "VSE Indic",
     "description": "Renders Indic Text in VSE",
     "author": "Oormi Creations",
-    "version": (0, 1, 1),
+    "version": (0, 2, 1),
     "blender": (2, 80, 0),
     "location": "Video Sequencer > VSE Indic",
     "warning": "", # used for warning icon and text in addons panel
@@ -39,9 +39,210 @@ import codecs
 import platform
 from datetime import datetime
 import pyvips
-
+import math
+from time import time
+from itertools import chain
+from math import *
 
 # Functions ############################################
+
+def setpixel(img,x,y,color, width):
+    offs = (x + int(y*width)) * 4
+    if offs >= len(img):
+        return img
+    for i in range(4):
+        img[offs+i] = color[i]
+    return img
+
+def blendpixel(img,x,y,color, width):
+    offs = (x + int(y*width)) * 4
+    if offs >= len(img):
+        return img
+    for i in range(4):
+        img[offs+i] += color[i]
+    return img
+
+
+def genCompGuides(pix, w, h, sz, cen, gold, third):
+    s = int(sz/2)
+    r = sz%2
+    #print(s, r)
+    if cen:
+        color = (0.5, 0.0, 0.0, 1.0 )
+        for z in range(-s, s+r):
+            for x in range(0,h):
+                pix = setpixel(pix, int(w/2) + z, x, color, w)
+        
+        for z in range(-s, s+r):
+            for x in range(0,w):
+                pix = setpixel(pix, x, int(h/2) + z, color, w)
+    
+    if third:    
+        color = (0.0, 0.5, 0.0, 1.0 )
+        for z in range(-s, s+r):
+            for x in range(0,h):
+                pix = setpixel(pix, int(w/3) + z, x, color, w)
+                pix = setpixel(pix, int(2*w/3) + z, x, color, w)
+                
+        for z in range(-s, s+r):
+            for x in range(0,w):
+                pix = setpixel(pix, x, int(h/3) + z, color, w)
+                pix = setpixel(pix, x, int(2*h/3) + z, color, w)
+
+    if gold:
+        color = (0.5, 0.5, 0.0, 1.0 )
+        gw = int(w/1.618033987)
+        for z in range(-s, s+r):
+            for x in range(0,h):
+                pix = setpixel(pix, gw + z, x, color, w)
+                pix = setpixel(pix, w - gw + z, x, color, w)
+
+        gh = int(h/1.618033987)
+        for z in range(-s, s+r):
+            for x in range(0,w):
+                pix = setpixel(pix, x, gh + z, color, w)
+                pix = setpixel(pix, x, h - gh + z, color, w)
+
+    return pix
+
+def genvig(pix, w, h, sz, dark, corner, vcol):
+    
+    if corner:
+        sz = sqrt(sz)/7.5
+        max = pow(w*h/4, dark)
+        for x in range(0, int(w/2)+1):
+            for y in range(0,int(h/2)+1):
+                fac = pow(x*y, dark/sz)/max
+                if fac<1.0:#speeds up 2X
+                    color = (vcol[0], vcol[1], vcol[2], 1 - fac)
+                    pix = setpixel(pix, x, y, color, w)
+                    pix = setpixel(pix, x, h-y, color, w)
+                    pix = setpixel(pix, w-x, y, color, w)
+                    pix = setpixel(pix, w-x, h-y, color, w)
+
+    else:
+        sz = sz*h/200
+        for z in range(0, int(sz)):
+            color = (vcol[0], vcol[1], vcol[2], dark*(1.0-z/sz) )
+            for x in range(0,h):
+                pix = setpixel(pix, z, x, color, w)
+                pix = blendpixel(pix, w-z, x, color, w)
+
+        for z in range(0, int(sz)):
+            color = (vcol[0], vcol[1], vcol[2], dark*(1.0-z/sz) )
+            for x in range(0,w):
+                pix = blendpixel(pix, x, z, color, w)
+                pix = blendpixel(pix, x, h-z, color, w)    
+    
+    return pix
+
+
+def genshape(pix, w, h, col, round):
+    
+    round = h*round/200
+    if h>w:
+        round = w*round/200
+    r = round*round
+
+    for x in range(0, int(w)+1):
+        for y in range(0,int(h)+1):
+            color = (col[0], col[1], col[2], 1)
+            pix = setpixel(pix, x, y, color, w)
+                
+    for x in range(0, int(round)+1):
+        for y in range(0,int(round)+1):
+            x1 = x- round
+            y1 = y- round
+            fac = (x1*x1 + y1*y1)
+            if fac>r:
+                color = (col[0], col[1], col[2], 0)
+                pix = setpixel(pix, x, y, color, w)
+                pix = setpixel(pix, w-x, y, color, w)
+                pix = setpixel(pix, x, h-y, color, w)
+                pix = setpixel(pix, w-x, h-y, color, w)
+            
+            #antialiasing
+            '''
+            if fac<r+160 and fac>r-160:
+                alfa = (r-fac)/fac
+                print(alfa)
+                color1 = (col[0], col[1], col[2], alfa*15)
+                pix = setpixel(pix, x, y, color1, w)
+            '''
+
+    return pix
+
+def createshape(intool, update):
+    dir = bpy.path.abspath("//")
+    outdir = dir + "vseindic"
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+            
+    today = datetime.now()
+    fname = today.strftime("%Y%m%d%H%M%S") + ".png"
+    
+    if update:
+        seq = bpy.context.scene.sequence_editor.active_strip
+        if seq is not None:
+            print(seq.type)
+            if seq.type != "IMAGE":
+                ShowMessageBox("Please select a strip of type 'Image'")
+                return 
+        else:
+            ShowMessageBox("Please select a strip")
+            return
+            
+        fname = seq.elements[0].filename
+
+    iname  = 'VSEIndicImageSh'
+    iwidth  = intool.in_shw #*bpy.context.scene.render.resolution_x/100
+    iheight = intool.in_shh #*bpy.context.scene.render.resolution_y/100
+    ialpha = True
+
+
+    oldImage = bpy.data.images.get(iname, None)
+    if oldImage:
+        bpy.data.images.remove(oldImage)
+
+    image = bpy.data.images.new(iname, iwidth, iheight, alpha=ialpha)
+
+    start = time()
+    pixels = list(image.pixels)
+    pixels = [0]*len(pixels)
+        
+    image.pixels = genshape(pixels, iwidth, iheight, intool.in_shcolor, intool.in_shr)
+    image.update()
+
+    print('T: %f seconds' % (time() - start))
+    
+    showimage(image)
+    
+    image.filepath_raw = "//vseindic/" + fname
+    image.file_format = 'PNG'
+    image.save()
+
+    dir = bpy.path.abspath("//")
+    outdir = dir + "vseindic"
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+        
+    if not update:
+        bpy.ops.sequencer.image_strip_add(directory= outdir + "/", 
+            files=[{"name":fname, "name":fname}], 
+            frame_start=bpy.context.scene.frame_current, 
+            frame_end=bpy.context.scene.frame_current+intool.in_dur, 
+            channel=intool.in_track)
+            
+    else:
+        bpy.ops.sequencer.refresh_all()
+        
+    seq = bpy.context.scene.sequence_editor.active_strip
+    seq.frame_final_duration = intool.in_dur
+    seq.use_translation = True
+    seq.transform.offset_x = (bpy.context.scene.render.resolution_x)/2 - (iwidth/2)
+    seq.transform.offset_y = (bpy.context.scene.render.resolution_y)/2 - (iheight/2)
+
+
 
 def ShowMessageBox(message = "", title = "VSE Indic Says...", icon = 'INFO'):
 
@@ -161,6 +362,13 @@ def importtext(tool, txtfile):
             id = str(n + 1)
             createindictext(tool, False, prefix + "_" + id)
 
+def showimage(image):
+    for area in bpy.context.screen.areas:
+        if area.type == 'IMAGE_EDITOR':
+            for space in area.spaces:
+                if space.type == 'IMAGE_EDITOR':
+                    space.image = image
+
 
 # Operators ###########################################
 
@@ -232,7 +440,160 @@ class CIM_OT_CImport(bpy.types.Operator):
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}    
 
+class CCG_OT_CCreateGuides(bpy.types.Operator):
+    bl_idname = "create.guides"
+    bl_label = "Create Guides"
+    bl_description = "Create guide lines strip."
 
+    def execute(self, context):
+        scene = context.scene
+        intool = scene.in_tool
+        
+        iname  = 'VSEIndicImage'
+        iwidth  = bpy.context.scene.render.resolution_x
+        iheight = bpy.context.scene.render.resolution_y
+        ialpha = True
+        linesz = intool.in_linesz #px
+
+        oldImage = bpy.data.images.get(iname, None)
+        if oldImage:
+            bpy.data.images.remove(oldImage)
+
+        image = bpy.data.images.new(iname, iwidth, iheight, alpha=ialpha)
+
+        start = time()
+        pixels = list(image.pixels)
+        pixels = [0]*len(pixels)
+            
+        image.pixels = genCompGuides(pixels, iwidth, iheight, linesz, intool.in_gcenter, intool.in_ggolden, intool.in_gthirds)
+        image.update()
+
+        print('T: %f seconds' % (time() - start))
+        
+        showimage(image)
+        
+        dir = bpy.path.abspath("//")
+        outdir = dir + "vseindic"
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        
+        image.filepath_raw = "//vseindic/VSEIndicGuides.png"
+        image.file_format = 'PNG'
+        image.save()
+
+        dir = bpy.path.abspath("//")
+        outdir = dir + "vseindic"
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+            
+        seq = None
+        try:
+            seq = bpy.context.scene.sequence_editor.sequences_all['VSEIndicGuides.png']
+        except:
+            pass
+                    
+        if seq is None:
+            bpy.ops.sequencer.image_strip_add(directory= outdir + "/", 
+                files=[{"name":"VSEIndicGuides.png", "name":"VSEIndicGuides.png"}], 
+                frame_start=bpy.context.scene.frame_start, 
+                frame_end=bpy.context.scene.frame_end)
+        else:
+            bpy.ops.sequencer.refresh_all()
+                                
+        return{'FINISHED'}
+    
+
+class CCV_OT_CCreateVig(bpy.types.Operator):
+    bl_idname = "create.vig"
+    bl_label = "Create Vignette"
+    bl_description = "Create vignette strip."
+
+    def execute(self, context):
+        scene = context.scene
+        intool = scene.in_tool
+        
+        iname  = 'VSEIndicImageVig'
+        iwidth  = bpy.context.scene.render.resolution_x
+        iheight = bpy.context.scene.render.resolution_y
+        ialpha = True
+        vigsz = intool.in_vigsz
+        dark = intool.in_vigdark/75
+        corner = intool.in_vigtype == 'COR'
+        vigcolor = intool.in_vigcolor
+
+        oldImage = bpy.data.images.get(iname, None)
+        if oldImage:
+            bpy.data.images.remove(oldImage)
+
+        image = bpy.data.images.new(iname, iwidth, iheight, alpha=ialpha)
+
+        start = time()
+        pixels = list(image.pixels)
+        pixels = [0]*len(pixels)
+            
+        image.pixels = genvig(pixels, iwidth, iheight, vigsz, dark, corner, vigcolor)
+        image.update()
+
+        print('T: %f seconds' % (time() - start))
+        
+        showimage(image)
+        
+        dir = bpy.path.abspath("//")
+        outdir = dir + "vseindic"
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+        
+        image.filepath_raw = "//vseindic/VSEIndicVig.png"
+        image.file_format = 'PNG'
+        image.save()
+
+        dir = bpy.path.abspath("//")
+        outdir = dir + "vseindic"
+        if not os.path.exists(outdir):
+            os.mkdir(outdir)
+            
+        seq = None
+        try:
+            seq = bpy.context.scene.sequence_editor.sequences_all['VSEIndicVig.png']
+        except:
+            pass
+                    
+        if seq is None:
+            bpy.ops.sequencer.image_strip_add(directory= outdir + "/", 
+                files=[{"name":"VSEIndicVig.png", "name":"VSEIndicVig.png"}], 
+                frame_start=bpy.context.scene.frame_start, 
+                frame_end=bpy.context.scene.frame_end)
+        else:
+            bpy.ops.sequencer.refresh_all()
+                                
+        return{'FINISHED'}
+
+class CCS_OT_CCreateShape(bpy.types.Operator):
+    bl_idname = "create.shape"
+    bl_label = "Create Shape"
+    bl_description = "Create Shapes."
+
+    def execute(self, context):
+        scene = context.scene
+        intool = scene.in_tool
+        createshape(intool, False)
+        
+
+        return{'FINISHED'}
+
+class CUS_OT_CUpdateShape(bpy.types.Operator):
+    bl_idname = "update.shape"
+    bl_label = "Update Shape"
+    bl_description = "Update existing shapes."
+
+    def execute(self, context):
+        scene = context.scene
+        intool = scene.in_tool
+        createshape(intool, True)
+        
+
+        return{'FINISHED'}
+    
     
 # Panel ###############################################
     
@@ -300,6 +661,88 @@ class OBJECT_PT_InImpPanel(bpy.types.Panel):
         row = layout.row(align=True)
         row.prop(intool, "in_sep")
         row.operator("import.text", text = "Import Text", icon='TEXT')
+
+
+class OBJECT_PT_GuidesPanel(bpy.types.Panel):
+
+    bl_label = "Guides"
+    bl_idname = "OBJECT_PT_VSEIndicGuides"
+    bl_category = "VSE Indic"
+    bl_space_type = 'SEQUENCE_EDITOR'
+    bl_region_type = 'UI'
+    bl_parent_id = "OBJECT_PT_VSEIndic"
+    bl_order = 2
+
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        intool = scene.in_tool
+
+        
+        row = layout.row(align=True)
+        row.prop(intool, "in_gcenter")
+        row.prop(intool, "in_ggolden")
+        row.prop(intool, "in_gthirds")
+        
+        row = layout.row(align=True)
+        row.prop(intool, "in_linesz")        
+        row.operator("create.guides", text = "Create Guides", icon='ALIGN_FLUSH')
+
+
+
+class OBJECT_PT_VigPanel(bpy.types.Panel):
+
+    bl_label = "Vignette"
+    bl_idname = "OBJECT_PT_VSEIndicVig"
+    bl_category = "VSE Indic"
+    bl_space_type = 'SEQUENCE_EDITOR'
+    bl_region_type = 'UI'
+    bl_parent_id = "OBJECT_PT_VSEIndic"
+    bl_order = 3
+
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        intool = scene.in_tool
+
+        row = layout.row(align=True)
+        row.prop(intool, "in_vigtype")
+        row.prop(intool, "in_vigcolor")        
+        
+        row = layout.row(align=True)
+        row.prop(intool, "in_vigdark")
+        row.prop(intool, "in_vigsz")
+        
+        layout.operator("create.vig", text = "Create Vignette", icon='CLIPUV_HLT')
+
+class OBJECT_PT_ShapePanel(bpy.types.Panel):
+
+    bl_label = "Shapes"
+    bl_idname = "OBJECT_PT_VSEIndicShape"
+    bl_category = "VSE Indic"
+    bl_space_type = 'SEQUENCE_EDITOR'
+    bl_region_type = 'UI'
+    bl_parent_id = "OBJECT_PT_VSEIndic"
+    bl_order = 4
+
+    
+    def draw(self, context):
+        layout = self.layout
+        scene = context.scene
+        intool = scene.in_tool
+
+        row = layout.row(align=True)
+        row.prop(intool, "in_shw")
+        row.prop(intool, "in_shh")        
+        
+        row = layout.row(align=True)
+        row.prop(intool, "in_shcolor")
+        row.prop(intool, "in_shr")
+        
+        layout.operator("create.shape", text = "Create Shape", icon='SOLO_ON')
+        layout.operator("update.shape", text = "Update Shape", icon='SOLO_OFF')
         
 
 class OBJECT_PT_InHelpPanel(bpy.types.Panel):
@@ -310,7 +753,7 @@ class OBJECT_PT_InHelpPanel(bpy.types.Panel):
     bl_space_type = 'SEQUENCE_EDITOR'
     bl_region_type = 'UI'
     bl_parent_id = "OBJECT_PT_VSEIndic"
-    bl_order = 2
+    bl_order = 5
 #    bl_options = "DEFAULT_CLOSED"
 #    bl_context = "objectmode"
 
@@ -429,6 +872,97 @@ class CCProperties(PropertyGroup):
         min = 1
       )
 
+    in_gcenter: BoolProperty(
+        name = "Center Cross",
+        description = "Show center cross guides",
+        default = True
+    )
+
+    in_ggolden: BoolProperty(
+        name = "Golden Ratio",
+        description = "Show golden ratio guides",
+        default = True
+    )
+
+    in_gthirds: BoolProperty(
+        name = "Thirds",
+        description = "Show thirds guides",
+        default = True
+    )
+
+    in_linesz: IntProperty(
+        name = "Line Size",
+        description = "Guide line size in pixels",
+        default = 3,
+        min = 1,
+        max = 100
+      )
+
+    in_vigdark: FloatProperty(
+        name = "Density",
+        description = "Darkness of the vignette border",
+        default = 15,
+        min = 0.1,
+        max = 100.0
+      )
+
+    in_vigcolor: FloatVectorProperty(
+         name = "",
+         subtype = "COLOR",
+         size = 4,
+         min = 0.0,
+         max = 1.0,
+         default = (0.0,0.0,0.0,1.0)
+     )
+
+    in_vigsz: FloatProperty(
+        name = "Size",
+        description = "Vignette size in percent of image width",
+        default = 30,
+        min = 1,
+        max = 100
+      )
+
+    in_vigtype: EnumProperty(
+        items = [("BOX","Box",""),("COR","Corner","")],
+        default = "COR",
+        name = "Type"
+    ) 
+
+    in_shw: IntProperty(
+        name = "Width",
+        description = "Width of the shape in pixels",
+        default = 100,
+        min = 1,
+        max = 100000
+      )
+
+    in_shh: IntProperty(
+        name = "Height",
+        description = "Height of the shape in pixels",
+        default = 100,
+        min = 1,
+        max = 100000
+      )
+
+    in_shr: IntProperty(
+        name = "Roundedness",
+        description = "Roundedness of the shape in %",
+        default = 10,
+        min = 0,
+        max = 100
+      )
+
+    in_shcolor: FloatVectorProperty(
+         name = "",
+         subtype = "COLOR",
+         size = 4,
+         min = 0.0,
+         max = 1.0,
+         default = (0.5,0.0,0.0,1.0)
+     )
+
+
 # ------------------------------------------------------------------------
 #    Registration
 # ------------------------------------------------------------------------
@@ -441,7 +975,14 @@ classes = (
     CUC_OT_CUpdateCurrent,
     CIM_OT_CImport,
     OBJECT_PT_InImpPanel,
-    OBJECT_PT_InHelpPanel
+    OBJECT_PT_InHelpPanel,
+    OBJECT_PT_GuidesPanel,
+    OBJECT_PT_VigPanel,
+    CCG_OT_CCreateGuides,
+    CCV_OT_CCreateVig,
+    OBJECT_PT_ShapePanel,
+    CCS_OT_CCreateShape,
+    CUS_OT_CUpdateShape
 )
 
 def register():
